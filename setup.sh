fbf63e6ef28691e5824ef6c292fab218b0c85294
Bishnu Bidari <5136163+scvishnu7@users.noreply.github.com>
2021-08-09 13:53:12 +0545

    WIP update for latest ubuntu

--- setup.sh
+++ setup.sh
@@ -13,23 +13,48 @@ doVnc=false
 # make secure via and fail2ban and ufw
 makeSecure=true
 
+#hostname
+read -rp "Enter the hostname of the server:" hostname
+echo "$hostname" > /etc/hostname
+echo "127.0.0.1 $hostname" >> /etc/hosts
+
+
 # user
-username="ubuntu"
-password="z"
+read -rp "Enter the username of the new user account:" username
+
+promptForPassword
 
 # public key for putting into .ssh/authorized_keys
 # "ssh-rsa AAAAB3NzaC1................"
-pubkey=""
+read -rp "Enter the public key of the new user account:" pubkey
+
 
 # mariadb root password
-mariadbPassword="z"
+# mariadbPassword="z"
 
 # download path (via wget)
 # ensure / at end
-downloadPath="https://raw.githubusercontent.com/amnah/vps-setup-script/master/"
-
+# downloadPath="https://raw.githubusercontent.com/amnah/vps-setup-script/master/"
+downloadPath="https://raw.githubusercontent.com/scvishnu7/vps-setup-script/master"
 # ------------ end config -----------------
 
+# ------------ Helper Functions -----------
+function promptForPassword() {
+   PASSWORDS_MATCH=0
+   while [ "${PASSWORDS_MATCH}" -eq "0" ]; do
+       read -s -rp "Enter new UNIX password:" password
+       printf "\n"
+       read -s -rp "Retype new UNIX password:" password_confirmation
+       printf "\n"
+
+       if [[ "${password}" != "${password_confirmation}" ]]; then
+           echo "Passwords do not match! Please try again."
+       else
+           PASSWORDS_MATCH=1
+       fi
+   done 
+}
+# ----------------------------------------
 
 
 if $doSetup ; then
@@ -42,22 +67,24 @@ if $doSetup ; then
 
     # nano + other apps for add-apt-repository cmd
     # http://stackoverflow.com/a/16032073 - page deleted :(
-    apt-get -y install nano software-properties-common
+    apt-get -y install software-properties-common
 
     # update time
     #rm -f /etc/localtime
     #ln -s /usr/share/zoneinfo/America/New_York /etc/localtime
-    apt-get -y install ntp
-    service ntp restart
+    # apt-get -y install ntp
+    # service ntp restart
 
     # set up .bashrc for root (for sudo -i)
-    mv ~/.bashrc ~/.bashrc.bak
-    wget ${downloadPath}files/.bashrc -O ~/.bashrc
-    chmod 644 ~/.bashrc
+    echo -e "\n alias gst=\"git status\"\n alias du=\"du -h --max-depth=1\f" \n" >>~/.bashrc.bak
+    
+    #mv ~/.bashrc ~/.bashrc.bak
+    #wget ${downloadPath}files/.bashrc -O ~/.bashrc
+    #chmod 644 ~/.bashrc
 
     # prevent root login
     sed -i "s/PermitRootLogin yes/#PermitRootLogin yes/g" /etc/ssh/sshd_config
-    echo -e "\n\nPermitRootLogin no\nPasswordAuthentication no\n#AllowUsers username@(your-ip) username@(another-ip-if-any)" >> /etc/ssh/sshd_config
+    echo -e "\n\nPermitRootLogin no\nPasswordAuthentication yes\n" >> /etc/ssh/sshd_config
 
     # create user and set up .bashrc + ssh
     adduser --disabled-password --gecos "" $username
@@ -89,14 +116,14 @@ if $doWebServer ; then
     add-apt-repository -y ppa:git-core/ppa
     add-apt-repository -y ppa:ondrej/php
     add-apt-repository -y ppa:chris-lea/redis-server
-    apt-key adv --recv-keys --keyserver hkp://keyserver.ubuntu.com:80 0xF1656F24C74CD1D8
-    add-apt-repository 'deb [arch=amd64,i386,ppc64el] http://ftp.osuosl.org/pub/mariadb/repo/10.4/ubuntu xenial main'
+    sudo apt-key adv --fetch-keys 'https://mariadb.org/mariadb_release_signing_key.asc'
+    sudo add-apt-repository 'deb [arch=amd64,arm64,ppc64el] https://mirrors.xtom.com.hk/mariadb/repo/10.6/ubuntu focal main'
     apt-get update
+    apt-get -y purge apache2* libapache2* php5-*
     apt-get -y install lbzip2 unzip htop git redis-server curl nginx mcrypt memcached
     apt-get -y install php php-cli php-fpm php-mysql php-curl php-dev php-gd php-mbstring php-memcached php-xml php-intl
-    apt-get -y purge apache2* libapache2* php5-*
     DEBIAN_FRONTEND=noninteractive apt-get -y install mariadb-server mariadb-client
-    mysqladmin -u root password $mariadbPassword
+    # mysqladmin -u root password $mariadbPassword
 
     # install mongo
     # https://github.com/mongodb/mongo-php-driver/issues/138#issuecomment-184749966
@@ -107,20 +134,20 @@ if $doWebServer ; then
     #echo -e "\nextension=mongodb.so\n" >> /etc/php/7.1/fpm/php.ini
 
     # set up nginx
-    mv /etc/nginx/nginx.conf /etc/nginx/nginx.conf.bak
-    mv /etc/nginx/sites-available/default /etc/nginx/sites-available/default.bak
-    wget ${downloadPath}files/sites-available/default -O /etc/nginx/sites-available/default
-    wget ${downloadPath}files/sites-available/_common -O /etc/nginx/sites-available/_common
-    wget ${downloadPath}files/nginx.conf -O /etc/nginx/nginx.conf
+    # mv /etc/nginx/nginx.conf /etc/nginx/nginx.conf.bak
+    # mv /etc/nginx/sites-available/default /etc/nginx/sites-available/default.bak
+    # wget ${downloadPath}files/sites-available/default -O /etc/nginx/sites-available/default
+    # wget ${downloadPath}files/sites-available/_common -O /etc/nginx/sites-available/_common
+    # wget ${downloadPath}files/nginx.conf -O /etc/nginx/nginx.conf
 
     # set up data dir with symlinks to important directories
-    mkdir -p /data
+    mkdir -p /root/sites
     mkdir -p /var/www
-    ln -s /var/www /data/
-    ln -s /etc/nginx/nginx.conf /data/
-    ln -s /etc/nginx/sites-available /data/
-    ln -s /etc/nginx/sites-enabled /data/
-    ln -s /var/log/nginx /data/log
+    ln -s /var/www /root/sites
+    ln -s /etc/nginx/nginx.conf /root/sites
+    ln -s /etc/nginx/sites-available /root/sites
+    ln -s /etc/nginx/sites-enabled /root/sites
+    ln -s /var/log/nginx /root/sites/log
 
     # set up nginx logs and logrotate
     mkdir /var/log/nginx/_
@@ -132,6 +159,7 @@ if $doWebServer ; then
         cp /etc/fail2ban/jail.conf /etc/fail2ban/jail.local
         sed -i "s/\[nginx-http-auth\]/\[nginx-http-auth\]\nenabled = true/g" /etc/fail2ban/jail.local
         sed -i "s/\[nginx-botsearch\]/\[nginx-botsearch\]\nenabled = true/g" /etc/fail2ban/jail.local
+        echo "\[sshd\]\n enabled = true\n port    = ssh\n maxretry = 3\n findtime = 600\n bantime  = 86400" >> /etc/fail2ban/jail.local
         sed -i "s/nginx\//nginx\/*\//g" /etc/fail2ban/paths-common.conf
         service fail2ban restart
 
@@ -141,7 +169,7 @@ if $doWebServer ; then
     fi
 
     # restart nginx services
-    service php7.1-fpm restart
+    service php8.0-fpm restart
     service nginx restart
 
     # change owner and permissions
@@ -152,9 +180,9 @@ if $doWebServer ; then
     #find /var/www -type f -print0 | xargs -0 chmod 0664 # not needed because there are no files in there
 
     # download backup and site scripts
-    wget ${downloadPath}backup.sh  
-    wget ${downloadPath}startNode.sh -O /home/$username/startNode.sh
-    chmod 700 backup.sh
+    # wget ${downloadPath}backup.sh  
+    # wget ${downloadPath}startNode.sh -O /home/$username/startNode.sh
+    # chmod 700 backup.sh
 
     # display success message
     echo -e "------------------------------------------"
